9ccb2442b8133334d55004a002049e30f696b6a3
Jared Sutton <jpsutton@gmail.com>
2021-09-12 12:19:11 -0400

    Add support to track packages across runs, and move transaction into sync command. Check to see if files are present before attempting to add to sync transaction. Fix logic to remove older packages when newer ones are downloaded.

--- repomirror.py
+++ repomirror.py
@@ -6,12 +6,15 @@ import shutil
 import platform
 import subprocess
 import glob
+import json
 from pprint import pprint
 
 import pyalpm
 
 from pycman_config import PacmanConfig
 from mlargparser.mlargparser import MLArgParser
+
+import code
 
 class PacOptions:
   root = "./alpmroot"
@@ -28,11 +31,12 @@ class PacRepoMirror (MLArgParser):
   pac_tools = {
     "repo-add": None
   }
+
+  tracked = None
   
   argDesc = {
     "package_name": "Name of a package to mirror to the local repository",
     "repo_name": "Name of the repostiory from which to retreive the package",
-    "ignore_deps": "Do not mirror dependency packages",
   }
 
   @staticmethod
@@ -62,8 +66,9 @@ class PacRepoMirror (MLArgParser):
       if full_path is None:
         sys.stderr.write(f"ERROR: could not locate {tool} in the current path.\n")
 
-  def __update_localrepo_metadata__ (self):
-    p = subprocess.run([self.pac_tools['repo-add'], os.path.join(PacOptions.cachedir, "repo.db.tar.gz")] + glob.glob(os.path.join(PacOptions.cachedir, "*.pkg.*")))
+  def __update_localrepo_metadata__ (self, transaction):
+    file_list = [ os.path.join(PacOptions.cachedir, package.filename) for package in transaction.to_add ]
+    p = subprocess.run([self.pac_tools['repo-add'], "-R", os.path.join(PacOptions.cachedir, "repo.db.tar.gz")] + file_list)
     p.check_returncode()
 
   def __init__ (self):
@@ -76,9 +81,59 @@ class PacRepoMirror (MLArgParser):
     for db in self.handle.get_syncdbs():
       self.repos[db.name] = db
 
+    if self.tracked is None:
+      self.__read_tracked__()
+
     super().__init__()
 
-  def mirror_package (self, repo_name:str, package_name:str, ignore_deps=True):
+  def __del__ (self):
+    del self.handle
+
+  def __save_tracked__ (self):
+    if self.tracked and len(self.tracked):
+      with open("./tracked.json", "w") as outfile:
+        outfile.write(json.dumps(self.tracked))
+
+  def __read_tracked__ (self):
+    if os.path.exists("./tracked.json"):
+      with open("./tracked.json", "r") as infile:
+        self.tracked = json.loads(infile.read())
+    else:
+      self.tracked = list()
+
+  def sync (self):
+    for name, repo in self.repos.items():
+      repo.update(True)
+
+    # Prepare and execute the transaction to mirror the packages
+    transaction = self.handle.init_transaction(downloadonly=True, nodeps=True)
+    added_count = 0
+
+    try:
+      for record in self.tracked:
+        pkg = self.repos[record['repo']].get_pkg(record['name'])
+
+        if pkg is None:
+          sys.stderr.write(f"WARNING: {record['name']} is no longer present in repo {record['repo']}\n")
+        elif os.path.exists(os.path.join(PacOptions.cachedir, pkg.filename)):
+          sys.stderr.write(f"DEBUG: {record['name']} is already downloaded. Skipping.\n")
+        else:
+          transaction.add_pkg(pkg)
+          added_count += 1
+
+      if not added_count:
+        sys.stderr.write(f"DEBUG: no packages were added to sync transaction.\n")
+        return
+
+      transaction.prepare()
+      transaction.commit()
+    finally:
+      transaction.release()
+
+    # Update the local repo metadata
+    self.__update_localrepo_metadata__(transaction)
+
+  def add (self, repo_name:str, package_name:str, no_sync:bool = False):
     """ Mirror a package from the specified repository to the local repository """
 
     if repo_name not in self.repos:
@@ -89,14 +144,23 @@ class PacRepoMirror (MLArgParser):
     repo = self.repos[repo_name]
     repo.update(True)
 
-    # Prepare and execute the transaction to mirror the package
-    transaction = self.handle.init_transaction(downloadonly=True, nodeps=ignore_deps)
-    transaction.add_pkg(repo.get_pkg(package_name))
-    transaction.prepare()
-    transaction.commit()
+    pkg = repo.get_pkg(package_name)
 
-    # Update the local repo metadata
-    self.__update_localrepo_metadata__()
+    # Make sure the package exists in the repo
+    if pkg is None:
+      sys.stderr.write(f"ERROR: {package_name} was not found in repo {repo_name}\n")
+      sys.exit(2)
+
+    if not len(list(filter(lambda x: x['name'] == package_name, self.tracked))):
+      self.tracked.append({
+        "name": package_name,
+        "repo": repo_name,
+      })
+
+    self.__save_tracked__()
+
+    if not no_sync:
+      self.sync()
 
 if __name__ == '__main__':
   PacRepoMirror()
